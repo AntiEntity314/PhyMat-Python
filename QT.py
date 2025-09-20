a6148d633cc9ee8149d60e8e278bac721acb17d1
AntiEntity314 <entityevi@gmail.com>
2025-09-20 10:24:19 +0530

    Add files via upload

---
+++ QT.py
@@ -0,0 +1,12 @@
+import math
+
+V0 = float(input("Barrier Voltage:"))
+E = float(input("Energy of incident particle: "))
+a = float(input("Define width: "))
+Ediff = V0 - E
+hbar = 1.05457*pow(10,-34)
+k = (2*Ediff)**0.5
+hypsin = (math.sinh(k*a))**2
+Denom = 1 + (((V0**2)*hypsin)/4*E*Ediff)
+T = 1/ Denom
+print(T)
