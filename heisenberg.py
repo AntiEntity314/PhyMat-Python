f6ee4fb939e0f45768ff2090e7dac592650c02d3
AntiEntity314 <entityevi@gmail.com>
2026-01-01 17:59:21 +0530

    Added Documentation(comments)

--- heisenberg.py
+++ heisenberg.py
@@ -1,33 +1,33 @@
-
 def heisE():
-    decide = int(input("Press '0' to calculate energy variation \nPress '1' to calculate time variation:"))
+    decide = int(input("Press '0' to calculate energy variation \nPress '1' to calculate time variation: "))
     if (decide == 0):
         t = float(input("Enter variation in time: "))
-        power = int(input("Enter the power of 10 for t"))
-        E = (1/t*pow(10,power))*(h/4*pi)
+        power = int(input("Enter the power of 10 for t: "))
+        E = (1/t*pow(10,power))*(h/4*pi)                                    #This Function uses the formula: (del)E.(del)t >= h/4*pi
         print(f"Variation in Energy is:{E} Joules")
     elif(decide == 1):
-        E = float(input("Enter Variation in energy:"))
-        power = int(input("Enter the power of 10 for E:"))
+        E = float(input("Enter Variation in energy: "))
+        power = int(input("Enter the power of 10 for E: "))
         t=(1/E*pow(10, power)) * (h / 4 * pi)
         print(f"Variation in Time is:{t} seconds")
+        
 def heisdis():
-    decide = int(input("Press '0' to calculate displacement variation \nPress '1' to calculate momentum variation:"))
+    decide = int(input("Press '0' to calculate displacement variation \nPress '1' to calculate momentum variation: "))
     if(decide == 0):
-        p = float(input("Enter variation in momentum:"))
-        power = int(input("Enter the power of 10 for momentum:"))
+        p = float(input("Enter variation in momentum: "))
+        power = int(input("Enter the power of 10 for momentum: "))
         x = (1/p*pow(10,power))*(h/4*pi)
         print(f"Variation in Displacement is {x} metres")
-    elif (decide == 1):
-        x = float(input("Enter variation in displacement:"))
-        power = int(input("Enter the power of 10 for displacement:"))
+    elif (decide == 1):                                                     #This Function uses the formula: (del)p.(del)x >= h/4*pi
+        x = float(input("Enter variation in displacement: "))
+        power = int(input("Enter the power of 10 for displacement: "))
         p = (1 / x * pow(10, power)) * (h / 4 * pi)
         print(f"Variation in Momentum is {p} metres^2-seconds")
 
 h = 6.626*pow(10,-34)
-pi = 3.14159265358979323846
+pi = 3.14159265358979323846   #Pi defined to 20 decimal places for higher accuracy
 decide = 0
-method= int(input("Enter '0' to calculate Variation in Energy/Time or Enter '1' to calculate Variation in Displacement/momentum:"))
+method= int(input("Enter '0' to calculate Variation in Energy/Time or Enter '1' to calculate Variation in Displacement/momentum: "))
 if(method== 0):
     heisE()
 elif(method==1):
